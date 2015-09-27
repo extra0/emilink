c14ede944387d941df50e8d4cc30811646321b8f
Ростислав <extra0@mail.ru>
2015-09-27 23:07:53 +0300

    не сделан блюр по ховеру на главной

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(function(){
 
-	//
+	// вызов фенсибокса
+	$('.fancybox').fancybox();
+
+	// установка заднего фона на блок
+	$('.catalog__cell').hover(function(){
+		$(this).children('.catalog__cell-inner').css('background', 'url(../'+ $(this).children('.catalog__cell-bg').attr('src') +')');
+	});
 
 });
