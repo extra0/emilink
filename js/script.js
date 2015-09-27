cbcc580b17160d2a546f3b8631697e448e10fd1b
Ростислав <extra0@mail.ru>
2015-09-28 01:29:09 +0300

    главная готова

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ $(function(){
 	// вызов фенсибокса
 	$('.fancybox').fancybox();
 
-	// установка заднего фона на блок
+	// установка заднего фона на блок категорий на главной
 	$('.catalog__cell').hover(function(){
-		$(this).children('.catalog__cell-inner').css('background', 'url(../'+ $(this).children('.catalog__cell-bg').attr('src') +')');
+		$('.catalog__cell-inner-bg').attr('style', $(this).attr('style'));
 	});
+	
 
 });
