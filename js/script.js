2bf2a304407e2a813a6a81244deaa24207200a58
Ростислав <extra0@mail.ru>
2015-10-05 01:29:00 +0300

    страница суб категории

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@ $(function(){
 	$('.catalog__cell').hover(function(){
 		$('.catalog__cell-inner-bg').attr('style', $(this).attr('style'));
 	});
+
+	// ф-я разбивки на разряды
+	function numberWithCommas(x) {
+        return x.toString().replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g,"\$1 ");
+    }
 	
+	$('._replace').each(function(i, j){
+		$(this).html(numberWithCommas($(this).html()));
+	});
 
 });
