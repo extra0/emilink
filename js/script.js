72455b24c007b14bfdc8ff46c505bda94e815936
Ростислав <extra0@mail.ru>
2015-10-16 22:26:55 +0300

    страница корзины без калькулятора

--- js/script.js
+++ js/script.js
@@ -69,5 +69,11 @@ $(function(){
 		}
 	});
 
-
+	// тольо цифры в инпуте
+	$('.order__table-quantity-input').bind("change keyup input click", function() {
+		if (this.value.match(/[^0-9]/g, '')) {
+			this.value = this.value.replace(/[^0-9]/g, '');
+		}
+	});
+	
 });
